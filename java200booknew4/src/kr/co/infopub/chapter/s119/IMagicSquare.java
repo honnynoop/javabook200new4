ab6dd8a5b41aa7f9dafe1123e80760cd6154aefc
CHOCO\choco <honnynoop@naver.com>
2018-04-11 23:58:56 +0900

    2018 java 200 new source

---
+++ java200booknew4/src/kr/co/infopub/chapter/s119/IMagicSquare.java
@@ -0,0 +1,6 @@
+package kr.co.infopub.chapter.s119;
+// 100% 구현안된 메서드를 갖는 클래스 -> 인터페이스(추상클래스임)
+public interface IMagicSquare {
+	void make();     // 반드시 구현해야할 메서드
+	void print();    // 반드시 구현해야할 메서드
+}
